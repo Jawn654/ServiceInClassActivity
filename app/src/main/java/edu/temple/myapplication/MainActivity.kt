24ab1d39e0fefd080e09025549de027c6d0a2f5b
Jawn654 <tup93272@temple.edu>
2025-11-18 14:48:48 -0500

    Added Menu Buttons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -6,13 +6,23 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timerBinder: TimerService.TimerBinder
+    lateinit var countView: TextView
     var isConnected = false
 
+    private val handler = android.os.Handler(Looper.getMainLooper()) { msg ->
+        countView.text = msg.what.toString()
+        true
+    }
     val serviceConnection = object: ServiceConnection{
         override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
@@ -27,6 +37,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countView = findViewById(R.id.countView)
 
         bindService(
             Intent(this, TimerService::class.java),
@@ -35,12 +46,37 @@ class MainActivity : AppCompatActivity() {
         )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            if(!timerBinder.isRunning) timerBinder.start(100)
+            if(!timerBinder.isRunning) {
+                timerBinder.start(100)
+                
+            }
             else timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
             if(isConnected) timerBinder.stop()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.startButton ->{ if(!timerBinder.isRunning) {
+                timerBinder.start(100)
+
+            }
+            else timerBinder.pause() }
+
+            R.id.stopButton -> { timerBinder.stop() }
+
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
